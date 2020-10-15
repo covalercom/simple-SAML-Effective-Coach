28c1645d769422c8120a0b47ad9e2e554b979754
Daniel Wachtel <daniel.wachtel@allori.edu.au>
2020-10-15 16:02:32 +1100

    Add provider to config

--- lib/simple_saml/configuration.rb
+++ lib/simple_saml/configuration.rb
@@ -1,8 +1,9 @@
 module SimpleSaml
   class Configuration
-    attr_accessor :login_url, :cert_fingerprint, :name_identifier_format
+    attr_accessor :provider, :login_url, :cert_fingerprint, :name_identifier_format
 
     def initialize
+      @provider = nil
       @login_url = nil
       @cert_fingerprint = nil
       @name_identifier_format = nil
--- spec/simple_saml_spec.rb
+++ spec/simple_saml_spec.rb
@@ -5,12 +5,14 @@ RSpec.describe SimpleSaml do
 
   it "can be configured" do
     SimpleSaml.configure do |config|
+      config.provider = :beddoes
       config.login_url = "https://localhost:3000/login"
       config.cert_fingerprint = "AB:23:CH:54..."
       config.name_identifier_format = "urn:oasis:names..."
     end
 
     config = SimpleSaml.configuration
+    expect(config.provider).to eq(:beddoes)
     expect(config.login_url).to eq("https://localhost:3000/login")
     expect(config.cert_fingerprint).to eq("AB:23:CH:54...")
     expect(config.name_identifier_format).to eq("urn:oasis:names...")
