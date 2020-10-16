8d5dcf70dfba90ea5288a9c88c1b61a2a0c6101f
Daniel Wachtel <daniel.wachtel@allori.edu.au>
2020-10-16 14:17:39 +1100

    Delete time based spec

--- spec/response_spec.rb
+++ spec/response_spec.rb
@@ -1,6 +1,5 @@
 RSpec.describe SimpleSaml::Response do
   let(:request) { OpenStruct.new(base_url: "https://localhost:3000") }
-  let(:errors) { ["Invalid Audience. The audience https://lvh.me:3000, did not match the expected audience https://localhost:3000/auth/saml"] }
 
   before(:all) { set_configurations! }
 
@@ -9,7 +8,6 @@ RSpec.describe SimpleSaml::Response do
     response = SimpleSaml::Response.new(invalid_params, credentials)
 
     expect(response.valid?).to be false
-    expect(response.errors).to eq errors
     expect(response.xml).to start_with "<samlp:Response ID="
     expect(response.attributes["email"]).to eq ["daniel@beddoes.com.au"]
     expect(response.attributes["name"]).to eq ["Daniel Wachtel"]
