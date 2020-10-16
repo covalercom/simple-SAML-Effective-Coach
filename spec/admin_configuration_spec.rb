25b8239287a068cb3a9ffe76ac6ab8ed264491b8
Daniel Wachtel <daniel.wachtel@allori.edu.au>
2020-10-16 14:38:21 +1100

    Move config specs to new file

---
+++ spec/admin_configuration_spec.rb
@@ -0,0 +1,22 @@
+RSpec.describe SimpleSaml::AdminCredential do
+  before(:each) { SimpleSaml::AdminCredential.reset_config! }
+
+  it "configs are nil by default" do
+    config = SimpleSaml::AdminCredential.configuration
+
+    expect(config.provider).to be nil
+    expect(config.login_url).to be nil
+    expect(config.cert_fingerprint).to be nil
+    expect(config.name_identifier_format).to be nil
+  end
+
+  it "can be configured" do
+    set_configurations!
+    config = SimpleSaml::AdminCredential.configuration
+
+    expect(config.provider).to eq(:beddoes)
+    expect(config.login_url).to eq("https://auth.beddoes.com.au/login_url")
+    expect(config.cert_fingerprint).to eq("AB:23:CH:54...")
+    expect(config.name_identifier_format).to eq("urn:oasis:names...")
+  end
+end
--- spec/simple_saml_spec.rb
+++ spec/simple_saml_spec.rb
@@ -4,23 +4,4 @@ RSpec.describe SimpleSaml do
   it "has a version number" do
     expect(SimpleSaml::VERSION).not_to be nil
   end
-
-  it "configs are nil by default" do
-    config = SimpleSaml::AdminCredential.configuration
-
-    expect(config.provider).to be nil
-    expect(config.login_url).to be nil
-    expect(config.cert_fingerprint).to be nil
-    expect(config.name_identifier_format).to be nil
-  end
-
-  it "can be configured" do
-    set_configurations!
-    config = SimpleSaml::AdminCredential.configuration
-
-    expect(config.provider).to eq(:beddoes)
-    expect(config.login_url).to eq("https://auth.beddoes.com.au/login_url")
-    expect(config.cert_fingerprint).to eq("AB:23:CH:54...")
-    expect(config.name_identifier_format).to eq("urn:oasis:names...")
-  end
 end
